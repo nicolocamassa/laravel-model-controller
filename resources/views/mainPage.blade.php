9a3b3cf3cefad821fb8100fb2605fcc1fd6a84e0
nicolocamassa <camassanicolo03@gmail.com>
2024-02-14 16:30:42 +0100

    [UPDATE] print film info in mainPage

--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
     <div>
-        
+        @foreach ($movies as $movie)
+        <div class="d-flex">
+            <div class="mx-2"> {{ $movie->id }} </div>
+            <div> {{ $movie->title }} </div>
+        </div>
+            
+        @endforeach
     </div>
 @endsection
