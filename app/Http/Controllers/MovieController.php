020b0d570a73ea6252ba2bf556a7a6388d4e5990
nicolocamassa <camassanicolo03@gmail.com>
2024-02-14 16:25:43 +0100

    [UPDATE] print of db array

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class MovieController extends Controller
 {
     public function index(){
+        $movies = Movie::all();
+
+        dd($movies);
 		return view('mainPage');
 	}
 }
--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-    <!-- Styles -->
-    @vite('resources/js/app.js')
-
-</head>
-
-<body>
-
-    <main class="bg-light">
-       
-    </main>
-
-</body>
-
-</html>
+@section('content')
+    <div>
+        
+    </div>
+@endsection
