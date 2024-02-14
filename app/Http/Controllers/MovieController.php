b397e0f3a891a8d214ae27e0bbdb7fd7cdb3e7df
nicolocamassa <camassanicolo03@gmail.com>
2024-02-14 16:10:59 +0100

    [UPDATE] imported controller in web.php

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class MovieController extends Controller
 {
-    //
+    public function index(){
+		return view('mainPage');
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MovieController as MovieController; 
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [Controller::class, 'index']);
+Route::get('/', [MovieController::class, 'index']);
