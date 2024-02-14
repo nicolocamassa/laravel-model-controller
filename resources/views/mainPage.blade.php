22324ad1d961f4cc9f63484a14f9ba8e6ffffcdf
nicolocamassa <camassanicolo03@gmail.com>
2024-02-14 16:38:24 +0100

    [BONUS] print film info in cards

--- resources/views/mainPage.blade.php
+++ resources/views/mainPage.blade.php
@@ -2,12 +2,29 @@
 
 @section('content')
     <div>
-        @foreach ($movies as $movie)
-        <div class="d-flex">
-            <div class="mx-2"> {{ $movie->id }} </div>
-            <div> {{ $movie->title }} </div>
+
+        <div class="d-flex justify-content-center align-items-center vh-100">
+
+            <div class="container">
+                <div class="row">
+                    <div class="row">
+
+                        @foreach ($movies as $movie)
+                            <div class="card m-3" style="width: 18rem;">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $movie->title }} </h5>
+                                    <h6 class="card-subtitle mb-2 text-body-secondary">Id: {{ $movie->id }}</h6>
+                                    <p class="card-text">Titolo originale: {{ $movie->original_title }}</p>
+                                    <a href="#" class="card-link">Vedi dettagli</a>
+                                </div>
+                            </div>
+                        @endforeach
+
+                    </div>
+                </div>
+            </div>
         </div>
-            
-        @endforeach
+
+
     </div>
 @endsection
